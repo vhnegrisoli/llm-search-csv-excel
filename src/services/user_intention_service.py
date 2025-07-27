fc438ed301b113127128134084feccbeaf2612e5
vhnegrisoli <victorhugonegrisoli.ccs@hotmail.com>
2025-07-27 10:13:28 -0300

    refactor: update user intention service

--- src/services/user_intention_service.py
+++ src/services/user_intention_service.py
@@ -1,5 +1,4 @@
-from langchain_core.messages import SystemMessage, HumanMessage
-from src.llm.llm_integration import LLMIntegration
+from src.services.llm_service import LLMService
 from src.models.dataframe import DataframeType
 from src.llm.prompts import USER_INTENTION_PROMPT
 
@@ -7,12 +6,11 @@ from src.llm.prompts import USER_INTENTION_PROMPT
 class UserIntention:
 
     def __init__(self):
-        self._llm = LLMIntegration()
+        self._llm_service = LLMService()
 
     def define_user_input(self, user_input: str) -> DataframeType:
-        messages = [
-            SystemMessage(content=USER_INTENTION_PROMPT),
-            HumanMessage(content=user_input)
-        ]
-        response = self._llm.call_llm(messages=messages)
+        response = self._llm_service.call_llm(
+            system_prompt=USER_INTENTION_PROMPT,
+            user_prompt=user_input
+        )
         return DataframeType.TEXT if DataframeType.TEXT.value in response.content else DataframeType.IMAGE 
